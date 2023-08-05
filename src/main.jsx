ee1168dfb73b45362a043f510daabb16202f597d
Karan Sharma <100772820+Iamkaransharma7742@users.noreply.github.com>
2023-08-05 19:21:03 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import AdsComponent from '..AdsComponent.js';
+
+function App() {
+
+//Note provide dataAdSlot value of your data-ad-slot which is your ad unit no.
+    return (
+        <>
+            <h1>Place To show Google AdSense</h1>
+           <AdsComponent dataAdSlot='X7XXXXXX5X' />
+        </>
+
+    );
+}
+
+export default App;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
